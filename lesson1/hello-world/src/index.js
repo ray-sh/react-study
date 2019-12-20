09db49cec8a5467870676c42c3b409ad7ed86af0
leiding <rayding@outlook.com>
2019-12-20 22:24:36 +0800

    一个简单的clock页面

--- lesson1/hello-world/src/index.js
+++ lesson1/hello-world/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+/*这里的html写法会被编译成一个函数调用,
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello, world!'
+);
+*/
 
-//这里的html写法会被编译成一个函数调用,xxx.createElelent(h1, "hello world")
-const name = "jack"
-const h = 
-<div>
-    <h1>Hello world {name}</h1>
-    <h2>head 2</h2>
-</div>
-
-
-ReactDOM.render(h,
-    document.getElementById('root')
-)
+//react的本质就是一个render函数，render会智能计算dom，只更新页面上变化的部分
+function clock()
+{
+    const h = <div>
+        <h1>clock</h1>
+        <h2 class="clock">It's {new Date().toLocaleTimeString()}</h2>
+    </div>
+    ReactDOM.render(h,document.getElementById('root'))
+}
+setInterval(clock,1000)
--- lesson1/note.md
+++ lesson1/note.md
@@ -28,4 +28,4 @@ We suggest that you begin by typing:
 2. component之间信息的交换
   - 每个square有自己的value
   - Board保存整个棋盘的状态
-
+3. 完成一个应有还是不是很容易，要充分理解应用在每个状态下的数据，以及到下一个状态的过程中如何更新状态
