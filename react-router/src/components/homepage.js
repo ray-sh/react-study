29b25bf5f0632757c97ca97f81a8d60c24efc400
leiding <rayding@outlook.com>
2020-01-06 22:38:50 +0800

    init submit about study react router

---
+++ react-router/src/components/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>self learning</h1>
+      <p>Flux, react router study</p>
+    </div>
+  );
+}
+
+//TODO：学习export的语法
+export default HomePage;
---
+++ react-router/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import HomePage from "./components/homepage.js";
+// v1 STAR MATCH - Starting Template
+
+ReactDOM.render(<HomePage />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
