eba9e698654ddbca61c87946cab437cc1f28de20
leiding <rayding@outlook.com>
2020-01-06 22:40:25 +0800

    倒入render的一种简单的方法

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import HomePage from "./components/homepage.js";
+
 // v1 STAR MATCH - Starting Template
 
-ReactDOM.render(<HomePage />, document.getElementById("root"));
+render(<HomePage />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
